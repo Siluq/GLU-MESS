3c01040c9c99d940d3e968c77a32af12d9282ef6
Sam Boesjes <534268@student.glu.nl>
2021-06-29 12:48:44 +0200

    Updates

---
+++ precache-manifest.1d5bb2abef3a75616b9c530043bbeb20.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5afe0b41ad568389cd2d3159d073106f",
+    "url": "/GLU-MESS/index.html"
+  },
+  {
+    "revision": "016312933e282aed52b8",
+    "url": "/GLU-MESS/static/css/2.8163d445.chunk.css"
+  },
+  {
+    "revision": "016312933e282aed52b8",
+    "url": "/GLU-MESS/static/js/2.5cd923cb.chunk.js"
+  },
+  {
+    "revision": "bab979f0996c4f83a712fc8810462bad",
+    "url": "/GLU-MESS/static/js/2.5cd923cb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "34469ead3fa2c8bcc76e",
+    "url": "/GLU-MESS/static/js/main.30ac7892.chunk.js"
+  },
+  {
+    "revision": "4698789938c46ea42c90",
+    "url": "/GLU-MESS/static/js/runtime-main.728422cb.js"
+  }
+]);
